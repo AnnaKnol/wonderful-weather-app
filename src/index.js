b6477d35e84ceb6d6696a9f1fb92ac9a351f780f
AnnaKnol <83373222+AnnaKnol@users.noreply.github.com>
2021-06-06 15:01:35 +0200

    added unit conversion to links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+
   let days = [
     "Sunday",
     "Monday",
@@ -10,10 +11,12 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = date.getDay();
+
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -28,14 +31,20 @@ function showCurrentWeather(response) {
   let cityName = document.querySelector("h1");
   cityName.innerHTML = response.data.name;
 
+  celsiusTemperature = response.data.main.temp;
+  celsiusFeelingTemperature = response.data.main.feels_like;
+  celsiusMaxTemperature = response.data.main.temp_max;
+  celsiusMinTemperature = response.data.main.temp_min;
+
   let iconCode = response.data.weather[0].icon;
   let currentDescription = response.data.weather[0].description;
-  let currentTemperature = Math.round(response.data.main.temp);
-  let maxToday = Math.round(response.data.main.temp_max);
-  let minToday = Math.round(response.data.main.temp_min);
+  let currentTemperature = Math.round(celsiusTemperature);
+  let maxToday = Math.round(celsiusMaxTemperature);
+  let minToday = Math.round(celsiusMinTemperature);
   let humidity = response.data.main.humidity;
-  let feelingTemp = Math.round(response.data.main.feels_like);
+  let feelingTemp = Math.round(celsiusFeelingTemperature);
   let windSpeed = Math.round(response.data.wind.speed * 3.6);
+  let bodyElement = document.querySelector("body");
 
   if (response.data.rain === undefined) {
     precipitationElement.innerHTML = "";
@@ -43,7 +52,6 @@ function showCurrentWeather(response) {
     precipitationElement.innerHTML = `Precipitation: ${response.data.rain["1h"]}mm`;
   }
 
-  let bodyElement = document.querySelector("body");
   if (iconCode[2] === "d") {
     bodyElement.classList.add("day");
     bodyElement.classList.remove("night");
@@ -52,7 +60,7 @@ function showCurrentWeather(response) {
     bodyElement.classList.remove("day");
   }
 
-  currentEmojiElement.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  currentIconElement.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
   currentDescriptionElement.innerHTML = currentDescription;
   currentTemperatureElement.innerHTML = currentTemperature;
   maxTodayElement.innerHTML = maxToday;
@@ -81,6 +89,7 @@ function showCityWeather(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
   axios.get(apiUrl).then(showCurrentWeather);
+
   searchInput.value = "";
 }
 
@@ -96,13 +105,47 @@ function determinePosition(position) {
 
 function updateCurrentLocation(event) {
   event.preventDefault();
+
   navigator.geolocation.getCurrentPosition(determinePosition);
 }
 
-let currentEmojiElement = document.querySelector(".current-emoji img");
-let currentDescriptionElement = document.querySelector(
-  ".current-state .description"
-);
+function fahrenheitConversion(temperature) {
+  return (temperature * 9) / 5 + 32;
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+
+  let fahrenheitTemperature = fahrenheitConversion(celsiusTemperature);
+  let fahrenheitFeelingTemperature = fahrenheitConversion(
+    celsiusFeelingTemperature
+  );
+  let fahrenheitMaxTemperature = fahrenheitConversion(celsiusMaxTemperature);
+  let fahrenheitMinTemperature = fahrenheitConversion(celsiusMinTemperature);
+
+  currentTemperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  feelingTempElement.innerHTML = Math.round(fahrenheitFeelingTemperature);
+  maxTodayElement.innerHTML = Math.round(fahrenheitMaxTemperature);
+  minTodayElement.innerHTML = Math.round(fahrenheitMinTemperature);
+
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+
+  currentTemperatureElement.innerHTML = Math.round(celsiusTemperature);
+  feelingTempElement.innerHTML = Math.round(celsiusFeelingTemperature);
+  maxTodayElement.innerHTML = Math.round(celsiusMaxTemperature);
+  minTodayElement.innerHTML = Math.round(celsiusMinTemperature);
+
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+let currentIconElement = document.querySelector(".current-icon img");
+let currentDescriptionElement = document.querySelector(".description");
 let currentTemperatureElement = document.querySelector(".current-temperature");
 let maxTodayElement = document.querySelector("#max_today");
 let minTodayElement = document.querySelector("#min_today");
@@ -110,18 +153,23 @@ let humidityElement = document.querySelector("#humidity");
 let feelingTempElement = document.querySelector("#feeling_temp");
 let windSpeedElement = document.querySelector("#wind_speed");
 let precipitationElement = document.querySelector("#precipitation");
-let maxTomorrowElement = document.querySelector("#max_tomorrow");
-let minTomorrowElement = document.querySelector("#min_tomorrow");
-let maxInTwoDaysElement = document.querySelector("#max_in_two_days");
-let minInTwoDaysElement = document.querySelector("#min_in_two_days");
-let maxInThreeDaysElement = document.querySelector("#max_in_three_days");
-let minInThreeDaysElement = document.querySelector("#min_in_three_days");
-let maxInFourDaysElement = document.querySelector("#max_in_four_days");
-let minInFourDaysElement = document.querySelector("#min_in_four_days");
-let maxInFiveDaysElement = document.querySelector("#max_in_five_days");
-let minInFiveDaysElement = document.querySelector("#min_in_five_days");
-let maxInSixDaysElement = document.querySelector("#max_in_six_days");
-let minInSixDaysElement = document.querySelector("#min_in_six_days");
+// let maxTomorrowElement = document.querySelector("#max_tomorrow");
+// let minTomorrowElement = document.querySelector("#min_tomorrow");
+// let maxInTwoDaysElement = document.querySelector("#max_in_two_days");
+// let minInTwoDaysElement = document.querySelector("#min_in_two_days");
+// let maxInThreeDaysElement = document.querySelector("#max_in_three_days");
+// let minInThreeDaysElement = document.querySelector("#min_in_three_days");
+// let maxInFourDaysElement = document.querySelector("#max_in_four_days");
+// let minInFourDaysElement = document.querySelector("#min_in_four_days");
+// let maxInFiveDaysElement = document.querySelector("#max_in_five_days");
+// let minInFiveDaysElement = document.querySelector("#min_in_five_days");
+// let maxInSixDaysElement = document.querySelector("#max_in_six_days");
+// let minInSixDaysElement = document.querySelector("#min_in_six_days");
+
+let celsiusTemperature = null;
+let celsiusFeelingTemperature = null;
+let celsiusMaxTemperature = null;
+let celsiusMinTemperature = null;
 
 let searchForm = document.querySelector("#search_form");
 searchForm.addEventListener("submit", showCityWeather);
@@ -129,18 +177,10 @@ searchForm.addEventListener("submit", showCityWeather);
 let currentLocationForm = document.querySelector("#current_location_form");
 currentLocationForm.addEventListener("submit", updateCurrentLocation);
 
+let fahrenheitLink = document.querySelector("#fahrenheit_link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius_link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 showIndexWeather();
-
-//bonus feature: fake fahrenheit
-
-// function convertCelsius(event) {
-//   event.preventDefault();
-// }
-// let celsiusLink = document.querySelector(".celsius");
-// celsiusLink.addEventListener("click", convertCelsius);
-
-// function convertFahrenheit(event) {
-//   event.preventDefault();
-// }
-// let fahrenheitLink = document.querySelector(".fahrenheit");
-// fahrenheitLink.addEventListener("click", convertFahrenheit);
